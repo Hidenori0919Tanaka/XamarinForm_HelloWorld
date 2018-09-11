ba316c88d7f39ef0342333a4f5e0f9691cacef43
Hidenori Tanaka <hidenori091985@yahoo.co.jp>
2018-09-11 23:18:32 +0900

    Initialize Commit

---
+++ XamarinForms_HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms_HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            label.Text = "Welcome to Xamarin.Forms!";
+        }
+    }
+}
